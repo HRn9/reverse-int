b105640dc881e8d474d04fa820ad5abb0708367e
Mark Romanov <oxxo8898@gmail.com>
2023-02-19 13:13:03 +0100

    result

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  const arr = n.toString(10).split('').map(int => parseInt(int, 10))
+  const reversed = arr.reverse()
+  const str = reversed.join('')
+  const result = Number(str)
+  if (n < 0) {
+    return Number(str.slice(0, -3))
+  }
+  return result
 }
